4190df7ba3a4fd2393326a1d28b4eecd4e52a16c
Dương Thứ <thuddth2307004@fpt.edu.vn>
2024-03-22 21:47:10 +0700

    up

--- Java-Programing-1/src/Main.java
+++ Java-Programing-1/src/Main.java
@@ -7,7 +7,15 @@ import java.sql.PreparedStatement;// kế thuwfa hoàn toàn khi đã import
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] agrs) {
-
-
+        Person person = new Person("Duong Dinh Thu",2023,"0981300594");
+        System.out.println("Information :" + person.toString());
+        Person person1 = new Person();
+        person1.setName("Do Thi Thao");
+        person1.setBornYear(2005);
+        person1.setPhone("0123456789");
+        System.out.println("Information :"+ person1.toString());
+        person1.setName("Tran Minh Vu");
+        System.out.println("Information :"+ person1.toString());
+        System.out.println(person1.getName());
     }
 }
