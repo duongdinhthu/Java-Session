24190af07718fb59dd69eeece432ac7dfbdce7a0
Dương Thứ <thuddth2307004@fpt.edu.vn>
2024-03-29 21:00:41 +0700

    up

--- Java-Programing-1/src/edunextS9/PlayerTest.java
+++ Java-Programing-1/src/edunextS9/PlayerTest.java
@@ -4,8 +4,8 @@ public class PlayerTest {
     public static void main(String[]args){
         TournamentIncome tournamentIncome = new TournamentIncome("9","9","Thu");
         PlayerIncome playerIncome = new PlayerIncome("Thu",5000);
-        System.out.println(tournamentIncome.calculateGradeBonus(100));
-        System.out.println(playerIncome.calculateTax());
+        System.out.println("Thưởng giải đấu: " + tournamentIncome.calculateGradeBonus(100) + " $");
+        System.out.println("Thưởng cá nhân : " + playerIncome.calculateTax()+" $");
     }
 }
 
